60413be7cd3d948934cefa8ff10ad398fda3a750
Ankit kumar <smpratik888@gmail.com>
2024-12-05 06:40:38 +0000

    reduce

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -6,8 +6,8 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // const newNums = myNumers.map( (num) => { return num + 10})
 
 const newNums = myNumers
-                .map((num) => num * 10 )
+                .map((num) => num * 10 ) //result will be passed into second map
                 .map( (num) => num + 1)
-                .filter( (num) => num >= 40)
+                .filter( (num) => num >= 40)// chained// in filter true or false is returned
 
 console.log(newNums);
